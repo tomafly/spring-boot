4f3553287219638dc079c030233548128d61715f
tomafly <AnFei_Ma@163.com>
2019-07-18 00:35:18 +0800

    'first'

---
+++ src/main/java/com/anfeis/springbootweb/controller/WebController.java
@@ -0,0 +1,13 @@
+package com.anfeis.springbootweb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+    @RequestMapping("/hello")
+    public String HelloSpring(){
+        return "hello spring .....";
+    }
+}
